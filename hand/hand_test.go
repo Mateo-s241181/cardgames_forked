6df8c86c1d10a763d7b33a7748e27f840b9a52de
Mateo <mateobarac0712@gmx.de>
2025-02-02 15:33:44 +0100

    hant test added

---
+++ hand/hand_test.go
@@ -0,0 +1,24 @@
+package hand
+
+import (
+	"cardgames/card"
+	"fmt"
+)
+
+func ExampleHand_Add() {
+	h := New()
+	h.Add(card.New(card.Seven, card.Diamonds))
+	h.Add(card.New(card.Eight, card.Clubs))
+	h.Add(card.New(card.King, card.Hearts))
+
+	fmt.Printf("%s", h.String())
+
+	// Output:
+	// ┌───────┐ ┌───────┐ ┌───────┐
+	// │7      │ │8      │ │K      │
+	// │       │ │       │ │       │
+	// │   ♦   │ │   ♣   │ │   ♥   │
+	// │       │ │       │ │       │
+	// │      7│ │      8│ │      K│
+	// └───────┘ └───────┘ └───────┘
+}
